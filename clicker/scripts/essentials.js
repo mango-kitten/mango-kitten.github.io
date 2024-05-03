a967daed571fd727048a61323b7ea34d405bb0f1
mango-kitten <159407524+mango-kitten@users.noreply.github.com>
2024-05-03 08:37:08 -0500

    updated to prevent errors v0.4.4

--- clicker/scripts/essentials.js
+++ clicker/scripts/essentials.js
@@ -11,7 +11,7 @@ function updatetext() {
     }
     document.getElementById("moneydisp").innerHTML = `Money: ${String(dolor)}`
     document.getElementById("mpcdisp").innerHTML = `Money per Click ${String(mpc)}`
-    document.getElementById("cat1").innerHTML = `Buy a Cat $2000 (${String(bought[0])}/1)`
+    // document.getElementById("cat1").innerHTML = `Buy a Cat $2000 (${String(bought[0])}/1)`
     document.getElementById("mpc1").innerHTML = `Increase mpc $50 (${String(bought[1])}/6)`
     document.getElementById("mpc2").innerHTML = `Mpc lv2 $1000 (${String(bought[2])}/8)`
     document.getElementById("mpc3").innerHTML = `Mpc lv3 $10000 (${String(bought[3])}/8)`
