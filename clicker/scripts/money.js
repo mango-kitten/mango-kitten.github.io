a72c654612e660fa7e01e93d0efc657cde3a2a01
mango-kitten <159407524+mango-kitten@users.noreply.github.com>
2024-05-03 15:23:09 -0500

    added totalmade v0.5.2

--- clicker/scripts/money.js
+++ clicker/scripts/money.js
@@ -1,8 +1,9 @@
 var element = document.getElementById('moneybutton');
 
 function money() {
-    dolor = dolor + mpc
-    document.getElementById("moneydisp").innerHTML = `Money: ${String(dolor)}`
-  }
+    dolor = dolor + (mpc * prestigelevel)
+    totalmade = totalmade + (mpc * prestigelevel)
+    document.getElementById("moneydisp").innerHTML = `Yarn: ${parseCommas(dolor)}`
+}
 
 element.addEventListener('click', money);
