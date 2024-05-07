efabf81a93e857a7929469472335f034811be23a
mango-kitten <159407524+mango-kitten@users.noreply.github.com>
2024-05-07 10:36:02 -0500

    added main file for the catalogue

---
+++ clicker/scripts/catalogue.js
@@ -0,0 +1,69 @@
+function enterCat(id) {
+    currentamnt = parseInt(catgot[id])
+    if (currentamnt == 0) {
+        cataUnlock(id)
+    }
+    catgot.splice(id, 1, currentamnt+1)
+    amtid = `${String(id)}bcata`
+    document.getElementById(amtid).innerHTML = `Obtained: ${String(catgot[id])}`
+}
+function cataUnlock(id) {
+    topida = `${String(id)}acatt`
+    topidb = `${String(id)}bcatt`
+    botida = `${String(id)}acatd`
+    botidb = `${String(id)}bcatd`
+    imgida = `${String(id)}acati`
+    imgidb = `${String(id)}bcati`
+
+    if (document.getElementById(topida)) {
+        document.getElementById(topida).classList = "cattext"
+        document.getElementById(topidb).classList = "cattext hidden greytext"
+        document.getElementById(botida).classList = "cattext2"
+        document.getElementById(botidb).classList = "cattext2 hidden greytext"
+        document.getElementById(imgida).classList = "catimg"
+        document.getElementById(imgidb).classList = "catimg hidden"
+        amtid = `${String(id)}bcata`
+        document.getElementById(amtid).innerHTML = `Obtained: ${String(catgot[id])}`
+    } else {
+        console.log("Error: Catalogue Entry not found")
+    }
+}
+
+function cataLock(id) {
+    topida = `${String(id)}acatt`
+    topidb = `${String(id)}bcatt`
+    botida = `${String(id)}acatd`
+    botidb = `${String(id)}bcatd`
+    imgida = `${String(id)}acati`
+    imgidb = `${String(id)}bcati`
+
+    if (document.getElementById(topida)) {
+        document.getElementById(topida).classList = "cattext hidden"
+        document.getElementById(topidb).classList = "cattext greytext"
+        document.getElementById(botida).classList = "cattext2 hidden"
+        document.getElementById(botidb).classList = "cattext2 greytext"
+        document.getElementById(imgida).classList = "catimg hidden"
+        document.getElementById(imgidb).classList = "catimg"
+        amtid = `${String(id)}bcata`
+        document.getElementById(amtid).innerHTML = `Obtained: ${String(catgot[id])}`
+    } else {
+        console.log("Error: Catalogue Entry not found")
+    }
+}
+
+function resetCatalogue() {
+    catgot = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    localStorage.setItem("catgot",[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    updateCatalogue();
+}
+
+function loadCatalogue() {
+    if (catgot == [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]) {
+        for (let i=0;i<cats.length;) {
+            if (cats[i] > 0) {
+                console.log("Current cat found, logging.")
+                catgot.splice(cats[i], 1, 1)
+            }
+        }
+    }
+}
