2ffd01bd93510ee40e48e168e25eda2219dd66eb
mango-kitten <159407524+mango-kitten@users.noreply.github.com>
2024-05-08 10:37:32 -0500

    spacebar now presses button!

--- clicker/scripts/money.js
+++ clicker/scripts/money.js
@@ -1,9 +1,27 @@
 var element = document.getElementById('moneybutton');
 
 function money() {
-    dolor = dolor + (mpc * prestigelevel)
-    totalmade = totalmade + (mpc * prestigelevel)
+    dolor = dolor + (mpc)
+    totalmade = totalmade + (mpc)
     document.getElementById("moneydisp").innerHTML = `Yarn: ${parseCommas(dolor)}`
 }
 
 element.addEventListener('click', money);
+
+window.addEventListener('keydown', function(e) {
+    if(e.keyCode == 32 && e.target == document.body) {
+      e.preventDefault();
+    }
+  });
+
+
+document.body.onkeyup = function(e) {
+    if (e.key == " " ||
+        e.code == "Space" ||      
+        e.keyCode == 32      
+    ) {
+        e.preventDefault();
+        console.log("spacebar pressed")
+        money()
+    }
+  }
