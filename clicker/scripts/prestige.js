15f52bd113f7b942981907ecc621d99bcc7aa306
mango-kitten <159407524+mango-kitten@users.noreply.github.com>
2024-05-08 14:20:13 -0500

    added formula

--- clicker/scripts/prestige.js
+++ clicker/scripts/prestige.js
@@ -13,3 +13,23 @@ function prestige() {
 }
 
 element.addEventListener('click', prestige);
+
+
+function prestigeLevel(yarn) {
+    let yarnamt = 0
+    let level = 0
+    let remaining = 0
+    let coeff = 1.05
+    while (yarnamt <= yarn) {
+        yarnamt = yarnamt + ((coeff ** level) * 1000000)
+        if (coeff > 1.005) {
+            coeff = coeff - 0.00002
+        }
+        if (yarnamt <= yarn) {
+            level = level + 1
+        }
+    }
+    yarnamt = yarnamt + ((coeff ** level) * 1000000)
+    remaining = yarnamt - yarn
+    return [level, remaining]
+}
