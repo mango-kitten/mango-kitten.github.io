8a9414f237b620ec2c90d0a76e4b12fde348cdd9
mango-kitten <159407524+mango-kitten@users.noreply.github.com>
2024-05-09 14:16:54 -0500

    lots of backend

--- clicker/scripts/prestige.js
+++ clicker/scripts/prestige.js
@@ -1,3 +1,7 @@
+let prestigecnf = 0
+
+
+
 function prestigecheck() {
     if (cats[2] > 0) {
         document.getElementById("prestigeholder").classList = "prestige"
@@ -6,13 +10,13 @@ function prestigecheck() {
     }
 }
 
-var element = document.getElementById('prestigebutton');
+// var element = document.getElementById('prestigebutton');
 
-function prestige() {
-    console.log("This button doesnt do anything yet!")
-}
+// function prestige() {
+//     console.log("This button doesnt do anything yet!")
+// }
 
-element.addEventListener('click', prestige);
+// element.addEventListener('click', prestige);
 
 
 function prestigeLevel(yarn) {
@@ -20,15 +24,39 @@ function prestigeLevel(yarn) {
     let level = 0
     let remaining = 0
     let coeff = 1.05
+    let currentlevel = 0
     while (yarnamt <= yarn) {
         yarnamt = yarnamt + ((coeff ** level) * 1000000)
-        if (coeff > 1.005) {
-            coeff = coeff - 0.00002
+        if (coeff > 1.002) {
+            coeff = coeff - 0.0002
         }
         if (yarnamt <= yarn) {
             level = level + 1
+            currentlevel = yarnamt
         }
     }
     remaining = yarnamt - yarn
-    return [level, remaining]
+    return [level, remaining, currentlevel]
 }
+
+var prs = document.getElementById('prestigebutton');
+
+
+function doPrestige() {
+    prestigecnf = 1
+    document.getElementById('confirmprestige').classList = "prestigebutton"
+}
+
+prs.addEventListener('click', doPrestige);
+
+var prscnf = document.getElementById('confirmprestige')
+
+function confirmPrestige() {
+    if (prestigecnf == 1) {
+        document.getElementById('confirmprestige').classList = "prestigebutton hidden"
+        prestigecnf = 0
+        console.log("nothing happened, because this doesnt work yet")
+    }
+}
+
+prscnf.addEventListener('click', confirmPrestige)
