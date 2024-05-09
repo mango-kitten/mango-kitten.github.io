c1322b5d07bd636336520737885dc18de18054f6
mango-kitten <159407524+mango-kitten@users.noreply.github.com>
2024-05-09 14:17:09 -0500

    lots of prestige backend

--- clicker/scripts/essentials.js
+++ clicker/scripts/essentials.js
@@ -14,11 +14,22 @@ function updatetext() {
         mpc = 1;
     }
     document.getElementById("moneydisp").innerHTML = `Yarn: ${parseCommas(dolor)}`
-    document.getElementById("mpcdisp").innerHTML = `Yarn per Click ${parseCommas(mpc)}`
+    if (prestigeMulti == 1) {
+        document.getElementById("mpcdisp").innerHTML = `Yarn per Click ${parseCommas(mpc)}`
+    } else {
+        document.getElementById("mpcdisp").innerHTML = `Yarn per Click ${parseCommas(mpc/prestigeMulti)} x ${prestigeMulti}`
+    }
     document.getElementById("mpc1").innerHTML = `Increase ypc $50 (${String(bought[1])}/3)`
     document.getElementById("mpc2").innerHTML = `Ypc lv2 $1000 (${String(bought[2])}/8)`
     document.getElementById("mpc3").innerHTML = `Ypc lv3 $10000 (${String(bought[3])}/8)`
-    document.getElementById("cpsdisp").innerHTML = `Clicks per Second: ${String(cps)}`
+
+    if (prestigeCps == 0) {
+        document.getElementById("cpsdisp").innerHTML = `Clicks per Second: ${String(cps)}`
+    } else {
+        document.getElementById("cpsdisp").innerHTML = `Clicks per Second: ${String(cps-prestigeCps)} + ${prestigeCps}`
+    }
+
+
     if (bought[4]) {
         document.getElementById("mpc4").innerHTML = `Ypc lv4 $100000 (${String(bought[4])}/10)`
     } else {
@@ -27,17 +38,29 @@ function updatetext() {
     if (cats[0] == 0) {
         document.getElementById("catown1").innerHTML = "You have no cat in slot 1"
     } else {
-        document.getElementById("catown1").innerHTML = `You have a ${String(ctname[cats[0]])} Cat (+${String(mpcadd[cats[0]])} ypc)`
+        if (strayMulti == 1) {
+            document.getElementById("catown1").innerHTML = `You have a ${String(ctname[cats[0]])} Cat <br>+${String(mpcadd[cats[0]])} ypc`
+        } else {
+            document.getElementById("catown1").innerHTML = `You have a ${String(ctname[cats[0]])} Cat <br>+${String(mpcadd[cats[0]])} x ${strayMulti} ypc`
+        }
     }
     if (cats[1] == 0) {
         document.getElementById("catown2").innerHTML = "You have no cat in slot 2"
     } else {
-        document.getElementById("catown2").innerHTML = `You have a ${String(ctname[cats[1]])} Cat (+${String(mpcadd[cats[1]])} ypc)`
+        if (magicMulti == 1) {
+            document.getElementById("catown2").innerHTML = `You have a ${String(ctname[cats[1]])} Cat <br>+${String(mpcadd[cats[1]])} ypc`
+        } else {
+            document.getElementById("catown2").innerHTML = `You have a ${String(ctname[cats[1]])} Cat <br>+${String(mpcadd[cats[1]])} x ${magicMulti} ypc`
+        }
     }
     if (cats[2] == 0) {
         document.getElementById("catown3").innerHTML = "You have no cat in slot 3"
     } else {
-        document.getElementById("catown3").innerHTML = `You have a ${String(ctname[cats[2]])} Cat (+${String(mpcadd[cats[2]])} ypc\n+${String(catcps[cats[2]])} cps)`
+        if (fruitMulti == 1) {
+            document.getElementById("catown3").innerHTML = `You have a ${String(ctname[cats[2]])} Cat <br>+${String(mpcadd[cats[2]])} ypc<br>+${String(catcps[cats[2]])} cps`
+        } else {
+            document.getElementById("catown3").innerHTML = `You have a ${String(ctname[cats[2]])} Cat <br>+${String(mpcadd[cats[2]])} x ${fruitMulti} ypc<br>+${String(catcps[cats[2]])} x ${fruitMulti} cps`
+        }
         if (cps == 0) {
             console.log("Updating cps...")
             cps = catcps[cats[2]]
@@ -46,7 +69,11 @@ function updatetext() {
     if (cats[3] == 0) {
         document.getElementById("catown4").innerHTML = "You have no cat in slot 4"
     } else {
-        document.getElementById("catown4").innerHTML = `You have a ${String(ctname[cats[3]])} Cat (+${String(mpcadd[cats[3]])} ypc\n+${String(catcps[cats[3]])} cps)`
+        if (programMulti == 1) {
+            document.getElementById("catown4").innerHTML = `You have a ${String(ctname[cats[3]])} Cat <br>+${String(mpcadd[cats[3]])} ypc<br>+${String(catcps[cats[3]])} cps`
+        } else {
+            document.getElementById("catown4").innerHTML = `You have a ${String(ctname[cats[3]])} Cat <br>+${String(mpcadd[cats[3]])} x ${programMulti} ypc<br>+${String(catcps[cats[3]])} x ${programMulti} cps`
+        }
         if (cps == 0) {
             console.log("Updating cps...")
             cps = catcps[cats[3]]
@@ -147,7 +174,9 @@ function fastTickUpdate() {
     document.getElementById("totalyarntext").innerHTML = `You have made ${parseCommas(totalmade)} yarn ever`
     tickLevel = prestigeLevel(totalmade)[0]
     tickRemain = Math.floor(prestigeLevel(totalmade)[1])
+    tickMax = Math.floor(prestigeLevel(totalmade)[2])
     document.getElementById("prestigelevel").innerHTML = `You will be prestige level ${tickLevel} (+${tickLevel-prestigelevel})<br>${parseCommas(tickRemain)} to next level`
+    document.getElementById("prestigeprogress").value = ((totalmade-tickMax)/((totalmade+tickRemain)-tickMax))*1000
 }
 
 
@@ -212,3 +241,75 @@ function updateCatalogue() {
 // function fromStore(str) {
 //     return parseInt(str, 36)
 // }
+
+function buildYpc() {
+    let mpca = 1
+
+    mpca = mpca + (mpcadd[cats[0]]*strayMulti)
+    mpca = mpca + (mpcadd[cats[1]]*magicMulti)
+    mpca = mpca + (mpcadd[cats[2]]*fruitMulti)
+    mpca = mpca + (mpcadd[cats[3]]*programMulti)
+
+
+    mpca = mpca + (1*bought[1])
+    mpca = mpca + (2*bought[2])
+    mpca = mpca + (10*bought[3])
+    mpca = mpca + (100*bought[4])
+
+    mpca = mpca * prestigeMulti
+    return mpca
+}
+
+function buildCps() {
+    let cpsa = 0
+    
+    cpsa = cpsa + (catcps[cats[2]]*fruitMulti)
+    cpsa = cpsa + (catcps[cats[3]]*programMulti)
+    
+    cpsa = cpsa + prestigeCps
+
+    return cpsa
+}
+
+function buildPrestige() {
+    if (prestigelevel >= 1) {
+        prestigeMulti = 2
+    } else {
+        prestigeMulti = 1
+    }
+    if (prestigelevel >= 10) {
+        strayMulti = 9
+    } else {
+        strayMulti = 1
+    }
+    if (prestigelevel >= 20) {
+        prestigeCps = 1
+    } else {
+        prestigeCps = 0
+    }
+    if (prestigelevel >= 50) {
+        prestigeMulti = 4
+    }
+    if (prestigelevel >= 100) {
+        magicMulti = 9
+    } else {
+        magicMulti = 1
+    }
+    if (prestigelevel >= 150) {
+        prestigeMulti = 8
+    }
+    if (prestigelevel >= 200) {
+        prestigeCps = 6
+    }
+    if (prestigelevel >= 250) {
+        fruitMulti = 9
+    } else {
+        fruitMulti = 1
+    }
+    if (prestigelevel >= 300) {
+        prestigeMulti = 16
+    }
+    if (prestigelevel >= 350) {
+        strayMulti = 81
+    }
+}
