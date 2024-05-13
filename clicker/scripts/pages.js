667214063104ac59f4abddc08e90789863139e43
mango-kitten <159407524+mango-kitten@users.noreply.github.com>
2024-05-13 14:29:04 -0500

    added reset confirm

--- clicker/scripts/pages.js
+++ clicker/scripts/pages.js
@@ -1,9 +1,14 @@
+let resetconfirmval = 0
+
+
 function homepage() {
     document.getElementById("mainpage").classList = "mainpage"
     document.getElementById("prestigepage").classList = "prestigepage hidden"
     document.getElementById("adoptpage").classList = "adoptpage hidden"
     document.getElementById("cataloguepage").classList = "cataloguepage hidden"
     document.getElementById("changelog").classList = "changelog hidden"
+    document.getElementById("resetpage").classList = "resetpage hidden"
+    resetconfirmval = 0
 }
 
 function prestigepage() {
@@ -12,6 +17,8 @@ function prestigepage() {
     document.getElementById("adoptpage").classList = "adoptpage hidden"
     document.getElementById("cataloguepage").classList = "cataloguepage hidden"
     document.getElementById("changelog").classList = "changelog hidden"
+    document.getElementById("resetpage").classList = "resetpage hidden"
+    resetconfirmval = 0
 }
 
 function adoptpage() {
@@ -20,6 +27,8 @@ function adoptpage() {
     document.getElementById("adoptpage").classList = "adoptpage"
     document.getElementById("cataloguepage").classList = "cataloguepage hidden"
     document.getElementById("changelog").classList = "changelog hidden"
+    document.getElementById("resetpage").classList = "resetpage hidden"
+    resetconfirmval = 0
 }
 
 function catapage() {
@@ -28,6 +37,8 @@ function catapage() {
     document.getElementById("adoptpage").classList = "adoptpage hidden"
     document.getElementById("cataloguepage").classList = "cataloguepage"
     document.getElementById("changelog").classList = "changelog hidden"
+    document.getElementById("resetpage").classList = "resetpage hidden"
+    resetconfirmval = 0
 }
 
 
@@ -37,4 +48,47 @@ function changepage() {
     document.getElementById("adoptpage").classList = "adoptpage hidden"
     document.getElementById("cataloguepage").classList = "cataloguepage hidden"
     document.getElementById("changelog").classList = "changelog"
+    document.getElementById("resetpage").classList = "resetpage hidden"
+    resetconfirmval = 0
+}
+
+
+
+function resetconfirm() {
+    if (resetconfirmval == 1) {
+        dolor = 0;
+        localStorage.setItem("money", 0);
+        mpc = 1;
+        // localStorage.setItem("mpc", 1);
+        bought = [0, 0, 0, 0];
+        localStorage.setItem("bought", [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+        cats = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        catgot1 = 0
+        localStorage.setItem("cats", [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+        cps = 0
+        // localStorage.setItem("cps", 0)
+        prestigelevel = 0
+        localStorage.setItem("prestige", 0)
+        totalmade = 0
+        localStorage.setItem("total", 0)
+        catgot = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        localStorage.setItem("catgot",[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        document.getElementById("moneydisp").innerHTML = `Yarn: ${String(dolor)}`
+        document.getElementById("mpcdisp").innerHTML = `Yarn per Click: ${String(mpc)}`
+        updatetext();
+        updatebutton();
+        updateCatalogue();
+        resetconfirmval = 0
+    }
+}
+
+
+function resetstorage() {
+    resetconfirmval = 1
+    document.getElementById("mainpage").classList = "mainpage hidden"
+    document.getElementById("prestigepage").classList = "prestigepage hidden"
+    document.getElementById("adoptpage").classList = "adoptpage hidden"
+    document.getElementById("cataloguepage").classList = "cataloguepage hidden"
+    document.getElementById("changelog").classList = "changelog hidden"
+    document.getElementById("resetpage").classList = "resetpage"
 }
