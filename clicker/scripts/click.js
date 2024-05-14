c04777ad9e89d04dbc80d41972b2ee064fd52cbf
mango-kitten <159407524+mango-kitten@users.noreply.github.com>
2024-05-14 10:19:55 -0500

    fixed exponential click

--- clicker/scripts/click.js
+++ clicker/scripts/click.js
@@ -1,13 +1,16 @@
 var mouseIsDown = false;
 let autoclick = 0
 let e = 0
+let hold = 0
+
 
 let moneybutton = document.getElementById("moneybutton")
 
 
 moneybutton.addEventListener('mousedown', function() {
+  clearTimeout(hold)
   mouseIsDown = true;
-  setTimeout(function() {
+  hold = setTimeout(function() {
     if(mouseIsDown) {
       autoclick = setInterval(function() {
         money()
