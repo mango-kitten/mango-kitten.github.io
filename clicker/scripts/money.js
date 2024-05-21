d8c229f6634f6b70ccd4736fb12b803ab9cffda8
mango-kitten <159407524+mango-kitten@users.noreply.github.com>
2024-05-21 14:07:05 -0500

    manual click counter

--- clicker/scripts/money.js
+++ clicker/scripts/money.js
@@ -1,12 +1,18 @@
 var element = document.getElementById('moneybutton');
 
 function money() {
-    dolor = dolor + (mpc)
-    totalmade = totalmade + (mpc)
-    document.getElementById("moneydisp").innerHTML = `Yarn: ${parseCommas(dolor)}`
+    dolor = dolor + ((mpc)*manualclick)
+    totalmade = totalmade + ((mpc)*manualclick)
+    document.getElementById("moneydisp").innerHTML = `Yarn: ${parseCommas(Number(dolor))}`
 }
 
-element.addEventListener('click', money);
+element.addEventListener('click', function () {
+    money()
+    manualclicks = manualclicks + 1
+    if (manualclicks >= 1000) {
+        achstore = `${achstore.substring(0, 4)}1${achstore.substring(5)}`
+    }
+});
 
 window.addEventListener('keydown', function(e) {
     if(e.keyCode == 32 && e.target == document.body) {
