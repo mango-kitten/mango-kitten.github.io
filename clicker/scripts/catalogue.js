256b4a1e68c8bb18fbe543d25f265bd1b44122c6
mango-kitten <159407524+mango-kitten@users.noreply.github.com>
2024-06-11 16:19:34 -0500

    v0.8.1

--- clicker/scripts/catalogue.js
+++ clicker/scripts/catalogue.js
@@ -9,6 +9,7 @@ let rustle = document.getElementById("cardrustle")
 
 mangocat.addEventListener('click', function () {
     achstore = `${achstore.substring(0, 1)}1${achstore.substring(2)}`
+    game.achievements[41] = 1
 })
 
 schcat.addEventListener('click', function () {
@@ -18,33 +19,32 @@ schcat.addEventListener('click', function () {
 
     } else {
         achstore = `${achstore.substring(0, 2)}1${achstore.substring(3)}`
+        game.achievements[42] = 1
         document.getElementById("murdera").classList = "tooltipbot hidden"
         document.getElementById("murderb").classList = "tooltipbot"
     }
 })
 
 function enterCat(id) {
-    while (catgot.length < id) {
-        catgot.push(0)
-    }
-    if (isNaN(Number(catgot[id]))) {
-        catgot.splice(id, 1, 0)
-    }
-    currentamnt = parseInt(catgot[id])
-    if (currentamnt == 0) {
-        cataUnlock(id)
-    }
-    catgot.splice(id, 1, currentamnt+1)
+    // while (catgot.length < id) {
+    //     catgot.push(0)
+    // }
+    // if (isNaN(Number(catgot[id]))) {
+    //     catgot.splice(id, 1, 0)
+    // }
+    currentamnt = game.catgot[id]
+    cataUnlock(id)
+    // catgot.splice(id, 1, currentamnt+1)
     amtid = `${String(id)}bcata`
-    document.getElementById(amtid).innerHTML = `Obtained: ${String(catgot[id])}`
+    document.getElementById(amtid).innerHTML = `Obtained: ${String(game.catgot[id])}`
 }
 function cataUnlock(id) {
-    while (catgot.length < id) {
-        catgot.push(0)
-    }
-    if (isNaN(Number(catgot[id]))) {
-        catgot.splice(id, 1, 0)
-    }
+    // while (catgot.length < id) {
+    //     catgot.push(0)
+    // }
+    // if (isNaN(Number(catgot[id]))) {
+    //     catgot.splice(id, 1, 0)
+    // }
     topida = `${String(id)}acatt`
     topidb = `${String(id)}bcatt`
     botida = `${String(id)}acatd`
@@ -59,12 +59,12 @@ function cataUnlock(id) {
         document.getElementById(botidb).classList = "cattext2 hidden greytext"
         document.getElementById(imgida).classList = "catimg"
         document.getElementById(imgidb).classList = "catimg hidden"
-        if (id == 9 && achievements[42]) {
+        if (id == 9 && game.achievements[42]== 1) {
             document.getElementById("9acati").classList = "catimg hidden"
             document.getElementById("9ccati").classList = "catimg"
         }
         amtid = `${String(id)}bcata`
-        document.getElementById(amtid).innerHTML = `Obtained: ${String(catgot[id])}`
+        document.getElementById(amtid).innerHTML = `Obtained: ${String(game.catgot[id])}`
     } else {
         console.log("Error: Catalogue Entry not found")
     }
@@ -86,25 +86,26 @@ function cataLock(id) {
         document.getElementById(imgida).classList = "catimg hidden"
         document.getElementById(imgidb).classList = "catimg"
         amtid = `${String(id)}bcata`
-        document.getElementById(amtid).innerHTML = `Obtained: ${String(catgot[id])}`
-    } else {
-        console.log("Error: Catalogue Entry not found")
+        document.getElementById(amtid).innerHTML = `Obtained: ${String(game.catgot[id])}`
     }
 }
 
 function resetCatalogue() {
-    catgot = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    localStorage.setItem("catgot",[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    // catgot = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    // localStorage.setItem("catgot",[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    for (let i=0; i<game.catgot.length; i++) {
+        game.catgot[i] = 0
+    }
     updateCatalogue();
 }
 
 function loadCatalogue() {
-    console.log(cats.length)
-    if (cats.length > 0) {
-        for (let i=0;i<cats.length;i++) {
-            if (cats[i] > 0 && catgot[cats[i]] == 0) {
-                console.log("Current cat found, logging.")
-                catgot.splice(cats[i], 1, 1)
+    if (game.cats.length > 0) {
+        for (let i=0;i<game.catgot.length;i++) {
+            if (game.catgot[i] > 0) {
+                enterCat(i)
+                // catgot.splice(cats[i], 1, 1)
+            } else {
             }
         }
     }
