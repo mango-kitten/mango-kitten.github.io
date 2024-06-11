814467dd36d762cbdf3eb48cd8fe8b6a269efa45
mango-kitten <159407524+mango-kitten@users.noreply.github.com>
2024-06-11 16:21:04 -0500

    v0.8.1

--- clicker/scripts/pages.js
+++ clicker/scripts/pages.js
@@ -5,6 +5,7 @@ let resetconfirmval = 0
 let fishbone = document.getElementById("realfishbone")
 fishbone.addEventListener('click', function () {
     achstore = `${achstore.substring(0, 6)}1${achstore.substring(7)}`
+    game.achievements[46] = 1
 })
 
 
@@ -19,6 +20,8 @@ function homepage() {
     document.getElementById("tutorial").classList = "tutorialpage hidden"
     document.getElementById("offlinepage").classList = "offlinescreen hidden"
     document.getElementById("achievementpage").classList = "achievementpage hidden"
+    document.getElementById("saveexp").classList = "saveexp hidden"
+    document.getElementById("saveimp").classList = "saveexp hidden"
     resetconfirmval = 0
 }
 
@@ -32,6 +35,8 @@ function prestigepage() {
     document.getElementById("tutorial").classList = "tutorialpage hidden"
     document.getElementById("offlinepage").classList = "offlinescreen hidden"
     document.getElementById("achievementpage").classList = "achievementpage hidden"
+    document.getElementById("saveexp").classList = "saveexp hidden"
+    document.getElementById("saveimp").classList = "saveexp hidden"
     resetconfirmval = 0
 }
 
@@ -45,6 +50,8 @@ function adoptpage() {
     document.getElementById("tutorial").classList = "tutorialpage hidden"
     document.getElementById("offlinepage").classList = "offlinescreen hidden"
     document.getElementById("achievementpage").classList = "achievementpage hidden"
+    document.getElementById("saveexp").classList = "saveexp hidden"
+    document.getElementById("saveimp").classList = "saveexp hidden"
     resetconfirmval = 0
 }
 
@@ -58,6 +65,8 @@ function catapage() {
     document.getElementById("tutorial").classList = "tutorialpage hidden"
     document.getElementById("offlinepage").classList = "offlinescreen hidden"
     document.getElementById("achievementpage").classList = "achievementpage hidden"
+    document.getElementById("saveexp").classList = "saveexp hidden"
+    document.getElementById("saveimp").classList = "saveexp hidden"
     resetconfirmval = 0
 }
 
@@ -71,6 +80,8 @@ function changepage() {
     document.getElementById("tutorial").classList = "tutorialpage hidden"
     document.getElementById("offlinepage").classList = "offlinescreen hidden"
     document.getElementById("achievementpage").classList = "achievementpage hidden"
+    document.getElementById("saveexp").classList = "saveexp hidden"
+    document.getElementById("saveimp").classList = "saveexp hidden"
     resetconfirmval = 0
 }
 
@@ -84,6 +95,8 @@ function tutorial() {
     document.getElementById("tutorial").classList = "tutorialpage"
     document.getElementById("offlinepage").classList = "offlinescreen hidden"
     document.getElementById("achievementpage").classList = "achievementpage hidden"
+    document.getElementById("saveexp").classList = "saveexp hidden"
+    document.getElementById("saveimp").classList = "saveexp hidden"
     resetconfirmval = 0
 }
 
@@ -97,6 +110,8 @@ function achievementpage() {
     document.getElementById("tutorial").classList = "tutorialpage hidden"
     document.getElementById("offlinepage").classList = "offlinescreen hidden"
     document.getElementById("achievementpage").classList = "achievementpage"
+    document.getElementById("saveexp").classList = "saveexp hidden"
+    document.getElementById("saveimp").classList = "saveexp hidden"
     resetconfirmval = 0
 }
 
@@ -120,8 +135,59 @@ function resetconfirm() {
         localStorage.setItem("total", 0)
         catgot = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         localStorage.setItem("catgot",[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-        document.getElementById("moneydisp").innerHTML = `Yarn: ${String(dolor)}`
-        document.getElementById("mpcdisp").innerHTML = `Yarn per Click: ${String(mpc)}`
+
+        achstore = "0000000"
+        localStorage.setItem("achstore", achstore)
+        game = {
+            yarn: 0,
+            totalyarn: 0,
+            fishbones: 0,
+            cats: [
+                0,
+                0
+            ],
+            catgot: [
+                0,
+                0
+            ],
+            bought: [
+                0,
+                0
+            ],
+            achievements: [
+                0,
+                0
+            ]
+        }
+        
+        if (game.cats.length < catslots) {
+            while (game.cats.length < catslots) {
+                game.cats.push(0)
+            }
+        }
+        if (game.catgot.length < cattotal) {
+            while (game.catgot.length < cattotal) {
+                game.catgot.push(0)
+            }
+        }
+        if (game.bought.length < boughttotal) {
+            while (game.bought.length < boughttotal) {
+                game.bought.push(0)
+            }
+        }
+        if (game.achievements.length < achtotal) {
+            while (game.achievements.length < achtotal) {
+                game.achievements.push(0)
+            }
+        }
+        
+        
+        console.log(game)
+    
+        localStorage.setItem("json", JSON.stringify(game))
+
+        document.getElementById("moneydisp").innerHTML = `Yarn: ${String(game.yarn)}`
+        document.getElementById("mpcdisp").innerHTML = `Yarn per Click: ${String(game.totalyarn)}`
         updatetext();
         updatebutton();
         updateCatalogue();
@@ -139,5 +205,8 @@ function resetstorage() {
     document.getElementById("changelog").classList = "changelog hidden"
     document.getElementById("tutorial").classList = "tutorialpage hidden"
     document.getElementById("resetpage").classList = "resetpage"
+    document.getElementById("saveexp").classList = "saveexp hidden"
     document.getElementById("offlinepage").classList = "offlinescreen hidden"
+    document.getElementById("saveexp").classList = "saveexp hidden"
+    document.getElementById("saveimp").classList = "saveexp hidden"
 }
