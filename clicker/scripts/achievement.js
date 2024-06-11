531e8c2b98093f0b68104acc583d246624606701
mango-kitten <159407524+mango-kitten@users.noreply.github.com>
2024-06-11 16:19:17 -0500

    v0.8.1

--- clicker/scripts/achievement.js
+++ clicker/scripts/achievement.js
@@ -3,16 +3,59 @@ let storeamnt = 15
 let achrecent = 0
 let achlog = ""
 
+// if (game.achievements.length < achievementamnt) {
+//     while (game.achievements.length < achievementamnt) {
+//         game.achievements.push(1)
+//     }
+//     console.log("added more lines to 'achievements'")
+// } else {
+//     console.log("enough lines")
+// }
+
+
+
+// preloading
+function achLoad() {
+    i = 1
+    while (i < game.achievements.length) {
+        if (game.achievements[i] == 1) {
+            try {
+                let achnamea = `a${i}a`
+                let achnameb = `a${i}b`
+                document.getElementById(achnamea).classList = "achimg hidden"
+                document.getElementById(achnameb).classList = "achimg"
+            } catch (error) {
+
+            }
+        } else {
+            try {
+            let achnamea = `a${i}a`
+            let achnameb = `a${i}b`
+            document.getElementById(achnamea).classList = "achimg"
+            document.getElementById(achnameb).classList = "achimg hidden"
+            } catch (error) {
+
+            }
+        }
+        i = i+1
+    }
+}
+
+
 function getAchievement(id) {
-    achievements.splice(id, 1, 1)
+    // achievements.splice(id, 1, 1)
     let achnamea = `a${id}a`
     let achnameb = `a${id}b`
     document.getElementById(achnamea).classList = "achimg hidden"
     document.getElementById(achnameb).classList = "achimg"
+    game.achievements[id]= 1
 }
 
 
 function buildAch() {
+    if (achstore == null) {
+        achstore = ""
+    }
     if (achstore.length < storeamnt) {
         while (achstore.length < storeamnt) {
             achstore = `${achstore}0`
@@ -26,14 +69,14 @@ function buildAch() {
 function updateAchievements() {
     achlog = ""
     achrecent = 0
-    if (Number(totalmade) >= 1000 && achievements[1] == 0) {
+    if (Number(game.totalyarn) >= 1000 && game.achievements[1]== 0) {
         getAchievement(1)
         achrecent = achrecent + 1
         if (achrecent == 1) {
             achlog = "A Start"
         }
     }
-    if (Number(totalmade) >= 100000 && achievements[2] == 0) {
+    if (Number(game.totalyarn) >= 100000 && game.achievements[2]== 0) {
         getAchievement(2)
         achrecent = achrecent + 1
         if (achrecent == 1) {
@@ -42,7 +85,7 @@ function updateAchievements() {
             achlog = `${achlog}, Makin that Yarn`
         }
     }
-    if (Number(totalmade) >= 10000000 && achievements[3] == 0) {
+    if (Number(game.totalyarn) >= 10000000 && game.achievements[3]== 0) {
         getAchievement(3)
         achrecent = achrecent + 1
         if (achrecent == 1) {
@@ -51,7 +94,7 @@ function updateAchievements() {
             achlog = `${achlog}, Go Big - no - Bigger`
         }
     }
-    if (Number(totalmade) >= 1000000000 && achievements[4] == 0) {
+    if (Number(game.totalyarn) >= 1000000000 && game.achievements[4]== 0) {
         getAchievement(4)
         achrecent = achrecent + 1
         if (achrecent == 1) {
@@ -60,7 +103,7 @@ function updateAchievements() {
             achlog = `${achlog}, No End`
         }
     }
-    if (Number(totalmade) >= 1000000000000 && achievements[5] == 0) {
+    if (Number(game.totalyarn) >= 1000000000000 && game.achievements[5]== 0) {
         getAchievement(5)
         achrecent = achrecent + 1
         if (achrecent == 1) {
@@ -69,7 +112,7 @@ function updateAchievements() {
             achlog = `${achlog}, Yarn the World 'Round`
         }
     }
-    if (Number(totalmade) >= 1000000000000000 && achievements[6] == 0) {
+    if (Number(game.totalyarn) >= 1000000000000000 && game.achievements[6]== 0) {
         getAchievement(6)
         achrecent = achrecent + 1
         if (achrecent == 1) {
@@ -78,7 +121,7 @@ function updateAchievements() {
             achlog = `${achlog}, Why Stop?`
         }
     }
-    if (Number(totalmade) >= 1000000000000000000 && achievements[7] == 0) {
+    if (Number(game.totalyarn) >= 1000000000000000000 && game.achievements[7]== 0) {
         getAchievement(7)
         achrecent = achrecent + 1
         if (achrecent == 1) {
@@ -87,7 +130,7 @@ function updateAchievements() {
             achlog = `${achlog}, Toying with you`
         }
     }
-    if (Number(totalmade) >= 1000000000000000000000 && achievements[8] == 0) {
+    if (Number(game.totalyarn) >= 1000000000000000000000 && game.achievements[8]== 0) {
         getAchievement(8)
         achrecent = achrecent + 1
         if (achrecent == 1) {
@@ -96,7 +139,7 @@ function updateAchievements() {
             achlog = `${achlog}, Overachieving`
         }
     }
-    if (Number(totalmade) >= 1000000000000000000000000 && achievements[9] == 0) {
+    if (Number(game.totalyarn) >= 1000000000000000000000000 && game.achievements[9]== 0) {
         getAchievement(9)
         achrecent = achrecent + 1
         if (achrecent == 1) {
@@ -105,7 +148,7 @@ function updateAchievements() {
             achlog = `${achlog}, Its in the works`
         }
     }
-    if (Number(totalmade) >= 1000000000000000000000000000 && achievements[10] == 0) {
+    if (Number(game.totalyarn) >= 1000000000000000000000000000 && game.achievements[10]== 0) {
         getAchievement(10)
         achrecent = achrecent + 1
         if (achrecent == 1) {
@@ -114,7 +157,7 @@ function updateAchievements() {
             achlog = `${achlog}, And none for you`
         }
     }
-    if (Number(totalmade) >= 1000000000000000000000000000000 && achievements[11] == 0) {
+    if (Number(game.totalyarn) >= 1000000000000000000000000000000 && game.achievements[11]== 0) {
         getAchievement(11)
         achrecent = achrecent + 1
         if (achrecent == 1) {
@@ -123,7 +166,7 @@ function updateAchievements() {
             achlog = `${achlog}, Magical Number Nine`
         }
     }
-    if (Number(totalmade) >= 1000000000000000000000000000000000 && achievements[12] == 0) {
+    if (Number(game.totalyarn) >= 1000000000000000000000000000000000 && game.achievements[12]== 0) {
         getAchievement(12)
         achrecent = achrecent + 1
         if (achrecent == 1) {
@@ -138,7 +181,7 @@ function updateAchievements() {
 
 
 
-    if (catgot[1] > 0 && catgot[2] > 0 && catgot[3] > 0 && catgot[4] > 0 && catgot[5] > 0 && achievements[14] == 0) {
+    if (game.catgot[1]> 0 && game.catgot[2]> 0 && game.catgot[3]> 0 && game.catgot[4]> 0 && game.catgot[5]> 0 && game.achievements[14]== 0) {
         getAchievement(14)
         achrecent = achrecent + 1
         if (achrecent == 1) {
@@ -147,7 +190,7 @@ function updateAchievements() {
             achlog = `${achlog}, Stray Cataloguist`
         }
     }
-    if (catgot[6] > 0 && catgot[7] > 0 && catgot[8] > 0 && catgot[9] > 0 && achievements[15] == 0) {
+    if (game.catgot[6]> 0 && game.catgot[7]> 0 && game.catgot[8]> 0 && game.catgot[9]> 0 && game.achievements[15]== 0) {
         getAchievement(15)
         achrecent = achrecent + 1
         if (achrecent == 1) {
@@ -156,7 +199,7 @@ function updateAchievements() {
             achlog = `${achlog}, Ritualized Cataloguist`
         }
     }
-    if (catgot[10] > 0 && catgot[11] > 0 && catgot[12] > 0 && catgot[13] > 0 && catgot[14] > 0 && achievements[16] == 0) {
+    if (game.catgot[10]> 0 && game.catgot[11]> 0 && game.catgot[12]> 0 && game.catgot[13]> 0 && game.catgot[14]> 0 && game.achievements[16]== 0) {
         getAchievement(16)
         achrecent = achrecent + 1
         if (achrecent == 1) {
@@ -165,7 +208,7 @@ function updateAchievements() {
             achlog = `${achlog}, Fruity Cataloguist`
         }
     }
-    if (catgot[15] > 0 && catgot[16] > 0 && catgot[17] > 0 && catgot[18] > 0 && achievements[17] == 0) {
+    if (game.catgot[15]> 0 && game.catgot[16]> 0 && game.catgot[17]> 0 && game.catgot[18]> 0 && game.achievements[17]== 0) {
         getAchievement(17)
         achrecent = achrecent + 1
         if (achrecent == 1) {
@@ -174,7 +217,7 @@ function updateAchievements() {
             achlog = `${achlog}, Programming Cataloguist`
         }
     }
-    if (catgot[18] > 0 && catgot[19] > 0 && catgot[20] > 0 && catgot[21] > 0 && catgot[22] > 0 && catgot[23] > 0 && catgot[24] > 0 && catgot[25] > 0 && catgot[26] > 0 && catgot[27] > 0 && achievements[18] == 0) {
+    if (game.catgot[19]> 0 && game.catgot[20]> 0 && game.catgot[21]> 0 && game.catgot[22]> 0 && game.catgot[23]> 0 && game.catgot[24]> 0 && game.catgot[25]> 0 && game.catgot[26]> 0 && game.catgot[27]> 0 && game.catgot[28]> 0 && game.achievements[18]== 0) {
         getAchievement(18)
         achrecent = achrecent + 1
         if (achrecent == 1) {
@@ -185,7 +228,7 @@ function updateAchievements() {
     }
 
 
-    if (Number(prestigelevel) >= 1 && achievements[27] == 0) {
+    if (game.fishbones >= 1 && game.achievements[27]== 0) {
         getAchievement(27)
         achrecent = achrecent + 1
         if (achrecent == 1) {
@@ -194,7 +237,7 @@ function updateAchievements() {
             achlog = `${achlog}, Fish Bones, Fish Bones`
         }
     }
-    if (Number(prestigelevel) >= 20 && achievements[28] == 0) {
+    if (game.fishbones >= 20 && game.achievements[28]== 0) {
         getAchievement(28)
         achrecent = achrecent + 1
         if (achrecent == 1) {
@@ -203,7 +246,7 @@ function updateAchievements() {
             achlog = `${achlog}, Yummy yummy fish bones`
         }
     }
-    if (Number(prestigelevel) >= 150 && achievements[29] == 0) {
+    if (game.fishbones >= 150 && game.achievements[29]== 0) {
         getAchievement(29)
         achrecent = achrecent + 1
         if (achrecent == 1) {
@@ -212,7 +255,7 @@ function updateAchievements() {
             achlog = `${achlog}, Eat them up!`
         }
     }
-    if (Number(prestigelevel) >= 1000 && achievements[30] == 0) {
+    if (game.fishbones >= 1000 && game.achievements[30]== 0) {
         getAchievement(30)
         achrecent = achrecent + 1
         if (achrecent == 1) {
@@ -221,7 +264,7 @@ function updateAchievements() {
             achlog = `${achlog}, All Mine`
         }
     }
-    if (Number(prestigelevel) >= 10000 && achievements[31] == 0) {
+    if (game.fishbones >= 10000 && game.achievements[31]== 0) {
         getAchievement(31)
         achrecent = achrecent + 1
         if (achrecent == 1) {
@@ -230,7 +273,7 @@ function updateAchievements() {
             achlog = `${achlog}, Are you sure youre not a grizzly?`
         }
     }
-    if (Number(prestigelevel) >= 50000 && achievements[32] == 0) {
+    if (game.fishbones >= 50000 && game.achievements[32]== 0) {
         getAchievement(32)
         achrecent = achrecent + 1
         if (achrecent == 1) {
@@ -239,7 +282,7 @@ function updateAchievements() {
             achlog = `${achlog}, Why? Because why not.`
         }
     }
-    if (Number(prestigelevel) >= 100000 && achievements[33] == 0) {
+    if (game.fishbones >= 100000 && game.achievements[33]== 0) {
         getAchievement(33)
         achrecent = achrecent + 1
         if (achrecent == 1) {
@@ -248,7 +291,7 @@ function updateAchievements() {
             achlog = `${achlog}, In such a world, what isnt fish?`
         }
     }
-    if (Number(prestigelevel) >= 500000 && achievements[34] == 0) {
+    if (game.fishbones >= 500000 && game.achievements[34]== 0) {
         getAchievement(34)
         achrecent = achrecent + 1
         if (achrecent == 1) {
@@ -263,7 +306,7 @@ function updateAchievements() {
 
 
 
-    if (achstore[0] == "1" && achievements[40] == 0) {
+    if (achstore[0] == "1" && game.achievements[40]== 0) {
         getAchievement(40)
         achrecent = achrecent + 1
         if (achrecent == 1) {
@@ -272,7 +315,7 @@ function updateAchievements() {
             achlog = `${achlog}, Scammed`
         }
     }
-    if (achstore[1] == "1" && achievements[41] == 0) {
+    if (achstore[1] == "1" && game.achievements[41]== 0) {
         getAchievement(41)
         achrecent = achrecent + 1
         if (achrecent == 1) {
@@ -281,7 +324,7 @@ function updateAchievements() {
             achlog = `${achlog}, Mango Clicker`
         }
     }
-    if (achstore[2] == "1" && achievements[42] == 0) {
+    if (achstore[2] == "1" && game.achievements[42]== 0) {
         document.getElementById("9acati").classList = "catimg hidden"
         document.getElementById("9ccati").classList = "catimg"
         document.getElementById("murdera").classList = "tooltipbot hidden"
@@ -294,7 +337,7 @@ function updateAchievements() {
             achlog = `${achlog}, MURDERER`
         }
     }
-    if (achstore[3] == "1" && achievements[43] == 0) {
+    if (achstore[3] == "1" && game.achievements[43]== 0) {
         getAchievement(43)
         document.getElementById("wastea").classList = "tooltipbot hidden"
         document.getElementById("wasteb").classList = "tooltipbot"
@@ -305,7 +348,7 @@ function updateAchievements() {
             achlog = `${achlog}, Why waste time?`
         }
     }
-    if (achstore[4] == "1" && achievements[44] == 0) {
+    if (achstore[4] == "1" && game.achievements[44]== 0) {
         getAchievement(44)
         achrecent = achrecent + 1
         if (achrecent == 1) {
@@ -314,7 +357,7 @@ function updateAchievements() {
             achlog = `${achlog}, Clicky`
         }
     }
-    if (achstore[5] == "1" && achievements[45] == 0) {
+    if (achstore[5] == "1" && game.achievements[45]== 0) {
         getAchievement(45)
         achrecent = achrecent + 1
         if (achrecent == 1) {
@@ -323,7 +366,7 @@ function updateAchievements() {
             achlog = `${achlog}, That's Handy`
         }
     }
-    if (achstore[6] == "1" && achievements[46] == 0) {
+    if (achstore[6] == "1" && game.achievements[46]== 0) {
         getAchievement(46)
         document.getElementById("reala").classList = "tooltipbot hidden"
         document.getElementById("realb").classList = "tooltipbot"
@@ -334,7 +377,7 @@ function updateAchievements() {
             achlog = `${achlog}, Get Real`
         }
     }
-    if (achstore[7] == "1" && achievements[47] == 0) {
+    if (achstore[7] == "1" && game.achievements[47]== 0) {
         getAchievement(47)
         document.getElementById("betraya").classList = "tooltipbot hidden"
         document.getElementById("betrayb").classList = "tooltipbot"
@@ -345,7 +388,8 @@ function updateAchievements() {
             achlog = `${achlog}, Betrayal`
         }
     }
-    if (catgot[7] >= 666 && achievements[48] == 0) {
+    if (catgot[7] >= 666 && game.achievements[48]== 0) {
+        game.achievements[48] = 1
         getAchievement(48)
         document.getElementById("demona").classList = "tooltipbot hidden"
         document.getElementById("demonb").classList = "tooltipbot"
@@ -356,7 +400,8 @@ function updateAchievements() {
             achlog = `${achlog}, Summoning Ritual`
         }
     }
-    if (catgot[23] >= 1047 && achievements[49] == 0) {
+    if (catgot[23] >= 1047 && game.achievements[49]== 0) {
+        game.achievements[49] = 1
         getAchievement(49)
         document.getElementById("suna").classList = "tooltipbot hidden"
         document.getElementById("sunb").classList = "tooltipbot"
@@ -367,7 +412,7 @@ function updateAchievements() {
             achlog = `${achlog}, One Sun`
         }
     }
-    if (catgot[27] >= 1 && achievements[50] == 0) {
+    if (catgot[27] >= 1 && game.achievements[50]== 0) {
         getAchievement(50)
         document.getElementById("plutoa").classList = "tooltipbot hidden"
         document.getElementById("plutob").classList = "tooltipbot"
