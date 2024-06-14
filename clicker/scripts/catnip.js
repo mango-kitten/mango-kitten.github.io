58db816afd5454a8e261cc3ea52340d2f97c9521
mango-kitten <159407524+mango-kitten@users.noreply.github.com>
2024-06-13 22:18:48 -0500

    fixes again

--- clicker/scripts/catnip.js
+++ clicker/scripts/catnip.js
@@ -20,10 +20,13 @@ function gaincatnip(sec = 1) {
 
     if (game.catnip > 0) {
         catnipmulti = ((game.catnip-(0.0000050005*game.catnip*game.catnip))/100)+1
-        document.getElementById("catnipbox").classList = "catnipbox"
+        document.getElementById("catnipdisp").classList = "catnipdisp"
+        document.getElementById("catnipimg").classList = "catnipimg"
+
     } else {
         catnipmulti = 1
-        document.getElementById("catnipbox").classList = "catnipbox hidden"
+        document.getElementById("catnipdisp").classList = "catnipdisp hidden"
+        document.getElementById("catnipimg").classList = "catnipimg hidden"
     }
     document.getElementById("catnipdisp").innerHTML = `Catnip: ${game.catnip} (x${Math.round(catnipmulti*10000)/10000} yarn)`
 }
