c5191a47f48d7103eadf9e649d211f2c371e5e87
mango-kitten <159407524+mango-kitten@users.noreply.github.com>
2024-06-13 22:11:30 -0500

    catnip

---
+++ clicker/scripts/catnip.js
@@ -0,0 +1,29 @@
+if (game.catnip) {
+    console.log("catnip exists")
+} else {
+    game.catnip = 0
+}
+
+
+let catnipsc = 0
+let catnipmulti = 1
+let catnipmax = 99999
+
+function gaincatnip(sec = 1) {
+    if (game.fishbones >= 999999) {
+        catnipsc = 1
+    }
+    game.catnip = game.catnip + (catnipsc * sec)
+    if (game.catnip > catnipmax) {
+        game.catnip = catnipmax
+    }
+
+    if (game.catnip > 0) {
+        catnipmulti = ((game.catnip-(0.0000050005*game.catnip*game.catnip))/100)+1
+        document.getElementById("catnipdisp").classList = "catnipdisp"
+    } else {
+        catnipmulti = 1
+        document.getElementById("catnipdisp").classList = "catnipdisp hidden"
+    }
+    document.getElementById("catnipdisp").innerHTML = `Catnip: ${game.catnip} (x${Math.round(catnipmulti*10000)/10000} yarn)`
+}
